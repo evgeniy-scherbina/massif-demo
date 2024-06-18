3a85ad0a88eeb17559ecd19c365133ee8668d4b9
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-06-18 11:05:39 -0400

    Commit

---
+++ a.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+const int KB = 1000;
+const int MB = KB * 1000;
+const int GB = MB * 1000;
+
+int main(void)
+{
+   // allocate 100KB
+  int n = 100;
+  int* x[n];
+  for (int i = 0; i < n; i++) {
+    x[i] = malloc(KB);
+  }
+
+  // allocate 1MB
+  int* mb = malloc(MB);
+
+  // allocate 100KB
+  n = 100;
+  int* y[n];
+  for (int i = 0; i < n; i++) {
+     y[i] = malloc(KB);
+  }
+
+ return 0;
+}
